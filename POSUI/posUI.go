1b05bdc79c8f37abba9110323ef65a426300e92c
martinrocket <martin.ruble@gmx.com>
2017-04-24 16:38:04 -0600

    add struct for posFuncs paremeters

--- POSUI/posUI.go
+++ POSUI/posUI.go
@@ -22,19 +22,18 @@ const (
 var boxWidth int
 var boxHeight int
 var version = "ver " + Version.GetVersion()
-var menu posMenu.PosFuncs
+var menu = PosMenu.GetMainMenu()
 
 // BuildBox funtion is exported for use with main POS.go file.
 func BuildBox() {
 	boxWidth = 50
 	boxHeight = 20
 	fmt.Print(rTop, strings.Repeat(hLine, boxWidth), lTop, nL)
-	fmt.Print(vLine, strings.Repeat(space, boxWidth-len(version)), version, vLine, nL)
+	fmt.Print(vLine, space, strings.Repeat(space, boxWidth-len(version)-2), version, space, vLine, nL)
 	for i := 0; i < boxHeight; i++ {
-		fmt.Print("...")
+		fmt.Print(vLine, menu.NewCheck.Name, strings.Repeat(space, boxWidth-len(menu.NewCheck.Name)-1), space, vLine, nL)
 	}
 	fmt.Println()
+	fmt.Println(len(menu.NewCheck.Name), " ", menu.NewCheck.Name)
 
-	menu.ClockIn = "yes"
-	fmt.Println(menu)
 }
--- posMenu/posFuncs.go
+++ posMenu/posFuncs.go
@@ -1,8 +1,23 @@
-package posMenu
+package PosMenu
 
-type PosFuncs struct {
-	NewCheck    string "New Check"
-	RecallCheck string
-	ClockIn     string
-	ClockOut    string
+type posFuncs struct {
+	NewCheck    funcType
+	RecallCheck funcType
+	ClockIn     funcType
+	ClockOut    funcType
+}
+
+func GetMainMenu() posFuncs {
+	var menu posFuncs
+	menu.NewCheck.Name = "New Check"
+	menu.RecallCheck.Name = "Recal Check"
+	menu.ClockIn.Name = "Clock In"
+	menu.ClockOut.Name = "Clock Out"
+	return menu
+}
+
+type funcType struct {
+	Active  bool
+	Visible bool
+	Name    string
 }
